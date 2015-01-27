31554c81a9ae94d3645139f7e0b93e64dcb98bb6
Fredrik Sandberg <fredsg@axis.com>
2015-01-27 10:48:29 +0100

    Updating example to new json data structure

--- snapshot-multi-cams/SnapshotMultiCams.js
+++ snapshot-multi-cams/SnapshotMultiCams.js
@@ -3,20 +3,50 @@
 // nmap -sP -PA21,22,25,3389 192.168.1.0/24
 // or/and: arp -a -n
 
-var camelopard = require('camelopard');
-var _ = require('lodash');
+var camelopard  = require('camelopard'),
+    _           = require('lodash');
 
-camelopard.addCameraConfiguration({name: 'MycamA', brand: 'Axis', ip: '192.168.0.200', username: 'root', password: 'pass'});
-camelopard.addCameraConfiguration({name: 'MycamB', brand: 'Axis', ip: '192.168.0.150', username: 'root', password: 'pass'});
-camelopard.addCameraConfiguration({name: 'MycamV', brand: 'Axis', ip: '192.168.0.120', username: 'root', password: 'pass'});
-var cams = camelopard.getCameraConfigurations();
+var data = {
+  "cameras": [
+    {
+      "name": "Default Camera",
+      "brand": "Axis",
+      "ip": "192.168.0.150", 
+      "username": "root",
+      "password": "pass",
+      "snapshot": {
+        "resolution": "1024x768",
+        "compression": 30,
+        "rotation": 0,
+        "cameraNum": 1,
+        "downloadFolder": "files\/snapshots"
+      }
+    },
+    {
+      "name": "Default Camera",
+      "brand": "Axis",
+      "ip": "192.168.0.200", 
+      "username": "root",
+      "password": "pass",
+      "snapshot": {
+        "resolution": "1024x768",
+        "compression": 30,
+        "rotation": 0,
+        "cameraNum": 1,
+        "downloadFolder": "files\/snapshots"
+      }
+    }
+  ]
+};
+var cams = data.cameras;
 
 _.forEach(cams, function (conf) {
-    camelopard.image.download(conf, function (err, res) {
-      if (err !== undefined) {
-        console.log('Error!' + err)
-      } else {
-        console.log('Image file downloaded as: ' + res);
-      }
-  })
-})
+  camelopard.image.download(conf, function (err, res) {
+
+    if (err !== undefined) {
+      console.log('Error!' + err);
+    } else {
+      console.log('Image file downloaded as: ' + res);
+    }
+  });
+});
